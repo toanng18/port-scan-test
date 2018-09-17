a19a56f04bc691fe9fffc5d0203199a0adca4967
toanng18 <43356448+toanng18@users.noreply.github.com>
2018-09-17 15:27:51 -0700

    Update port-scan-1.py
    other user

--- port-scan-1.py
+++ port-scan-1.py
@@ -10,6 +10,8 @@ from datetime import datetime
 # Clear the screen
 subprocess.call('clear', shell=True)
 
+# other user 
+
 raw_input = 0
 # Ask for input
 remoteServer    = raw_input("Enter a remote host to scan: ")
